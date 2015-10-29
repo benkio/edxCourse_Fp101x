d0004b75e97966629f1423b25a4520a3c249175c
unknown <Enrico Benini@benini.apex-net.it>
2015-10-29 17:02:25 +0100

    Add some correction, now the scratch compiles

--- List Comprehension/scratch.hs
+++ List Comprehension/scratch.hs
@@ -1,4 +1,5 @@
-sum100 = sum [[x*x]] | x <- [1 .. 100]]
+
+--sum100 = sum [[x*x] | x<-[1 .. 100]]
 sum100' = sum [ x ^ 2 | x <- [1 .. 100]]
 sum100'' = sum [const 2 x | x <- [1 .. 100]]
 sum100''' = foldl (+) (1) [ x^2 | x <- [1 .. 100]]
@@ -14,10 +15,11 @@ pyths'' n = [(x,y,z) | x <- [1..n], y <- [1..n], z<-[1..n], x^2 + y^2 == z^2]
 pyths''' n = [(x,y, (x^2 + y ^ 2)) | x <- [1..n], y <- [1..n]]
 
 factors :: Int -> [Int]
+
 factors n = [x | x <-[1..n], n `mod` x == 0]
 
 perfects n = [x | x <- [1..n], isPerfect x]
   where isPerfect num = sum (factors num) == num
-perfects' n =
-perfects'' n =
-perfects''' n = 
+--perfects' n =
+--perfects'' n =
+--perfects''' n = 

d7982454530458053112dbe58f1cbe3e104142fb
unknown <Enrico Benini@benini.apex-net.it>
2015-11-05 18:55:27 +0100

    done first exercises of higher order functions

--- Higher-Order Functions/scratch.hs
+++ Higher-Order Functions/scratch.hs
@@ -19,3 +19,48 @@ myany5 p = null . filter p
 myany6 p xs = not (all (\x -> not (p x)) xs)
 myany7 p xs = foldr (\x acc -> (p x) || acc) False xs
 myany8 p xs = foldr (||) True (map p xs)
+
+-- TakeWhile functions
+
+myTakeWhile _ [] = []
+myTakeWhile p (x : xs)
+  | p x = x : myTakeWhile p xs
+  | otherwise = myTakeWhile p xs
+
+myTakeWhile2 _ [] = []
+myTakeWhile2 p (x : xs)
+  | p x = x : myTakeWhile2 p xs
+  | otherwise = []
+
+myTakeWhile3 _ [] = []
+myTakeWhile3 p (x : xs)
+  | p x = myTakeWhile3 p xs
+  | otherwise = []
+
+myTakeWhile4 p = foldl (\acc x -> if p x then x : acc else acc) []
+
+-- DropWhile functions
+
+myDropWhile _ [] = []
+myDropWhile p (x : xs)
+  | p x = myDropWhile p xs
+  | otherwise = x : xs
+
+myDropWhile2 _ [] = []
+myDropWhile2 p (x : xs)
+  | p x = myDropWhile2 p xs
+  | otherwise = xs
+
+myDropWhile3 p = foldr (\x acc -> if p x then acc else x : acc) []
+
+myDropWhile4 p = foldl add []
+  where add [] x = if p x then [] else [x]
+        add acc x = x : acc
+
+-- Map functions
+
+myMap f = foldr (\x xs -> xs ++ [f x]) []
+myMap2 f = foldr (\x xs -> f x ++ xs) []
+myMap3 f = foldl (\xs x -> f x : xs) []
+myMap4 f = foldl (\xs x -> xs ++ [f x]) []
+
